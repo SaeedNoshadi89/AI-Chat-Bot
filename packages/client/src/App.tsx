b04d29b6bb0ff4398b8cf96ed8710b9dd6d1126f
Saeed Noshadi <saeednoshadi89@gmail.com>
2025-09-09 00:05:10 +0330

    setup Husky

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -6,11 +6,12 @@ function App() {
    const [message, setMessage] = useState('');
 
    useEffect(() => {
-      fetch('/api/hello')
+     fetch('/api/hello')
          .then((res) => {
             if (!res.ok) throw new Error(`HTTP ${res.status}`);
             return res.json();
          })
+
          .then((data) => setMessage(data.message))
          .catch((err) => setMessage(`Error: ${err.message}`));
    }, []);
