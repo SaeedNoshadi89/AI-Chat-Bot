f45a1290de248b9d52783b909ae7614c2df3535d
Saeed Noshadi <saeednoshadi89@gmail.com>
2025-09-15 16:29:56 +0330

    refactor routing

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,21 +1,12 @@
 import express, { json } from 'express';
-import type { Request, Response } from 'express';
 import dotenv from 'dotenv';
-import { chatController } from './controllers/chat.controller';
+import router from './router';
 
 dotenv.config();
 const app = express();
 app.use(json());
+app.use(router);
 const port = process.env.PORT || 3000;
-
-app.get('/', (req: Request, res: Response) => {
-   res.send(process.env.OPENAI_API_KEY);
-});
-app.post('/api/chat', chatController.sendMessage);
-
-app.get('/api/hello', (req: Request, res: Response) => {
-   res.json({ message: 'Hello World!' });
-});
 
 app.listen(port, () => {
    console.log(`Server is running on http://localhost:${port}`);
